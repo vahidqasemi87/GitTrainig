8f4c92c854affe78d1763659677925000b83b176
Vahid <v.ghasemi87@gmail.com>
2021-06-25 19:27:07 +0430

    Final Read Excel

--- MyProgram/ReadExcell.cs
+++ MyProgram/ReadExcell.cs
@@ -1,9 +1,16 @@
-//http://www.dotnet-tutorials.net/Article/read-an-excel-file-in-csharp
+static void Main(string[] args)
+        {
+            //http://www.dotnet-tutorials.net/Article/read-an-excel-file-in-csharp
             var empList = new List<Employee>();
+
             // path to your excel file
+
+            string path = @"D:\pro\test.xlsx";
             FileInfo fileInfo = new FileInfo(path);
 
             ExcelPackage package = new ExcelPackage(fileInfo);
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
             ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
 
             // get number of rows and columns in the sheet
@@ -15,9 +22,17 @@
             {
                 for (int j = 1; j <= columns; j++)
                 {
-
                     //string content = worksheet.Cells[i, j].Value.ToString();
                     /* Do something ...*/
-                    empList.Add(new Employee {NationalCode= worksheet.Cells[i,j].Value.ToString()});
+                    empList.Add(new Employee { NationalCode = worksheet.Cells[i, j].Value.ToString() });
                 }
             }
+            Console.WriteLine(empList);
+            Console.ReadKey();
+        }
+    }
+    public class Employee
+    {
+        public string NationalCode { get; set; }
+        public int EmployeeStatus { get; set; }
+    }
